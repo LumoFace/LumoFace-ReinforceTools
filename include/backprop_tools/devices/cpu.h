eb7a44d9d74d6d3dbb51e94b6d7b5695b5018dab
LumoFace <lumoface@proton.me>
2022-07-29 18:17:15 +0000

    Version 1.9.2

--- include/backprop_tools/devices/cpu.h
+++ include/backprop_tools/devices/cpu.h
@@ -1,71 +1,62 @@
-#ifndef BACKPROP_TOOLS_DEVICES_ARM_H
-#define BACKPROP_TOOLS_DEVICES_ARM_H
+
+#ifndef BACKPROP_TOOLS_DEVICES_CPU_H
+#define BACKPROP_TOOLS_DEVICES_CPU_H
 
 #include <backprop_tools/utils/generic/typing.h>
 #include "devices.h"
 
 #include <cstddef>
 namespace backprop_tools::devices{
-    namespace arm{
+    namespace cpu{
         template <typename T_MATH, typename T_RANDOM, typename T_LOGGING>
         struct Specification{
+            using EXECUTION_HINTS = ExecutionHints;
             using MATH = T_MATH;
             using RANDOM = T_RANDOM;
             using LOGGING = T_LOGGING;
+            using index_t = size_t;
         };
         struct Base{
-            static constexpr DeviceId DEVICE_ID = DeviceId::ARM;
+            static constexpr DeviceId DEVICE_ID = DeviceId::CPU;
             using index_t = size_t;
-            static constexpr index_t MAX_INDEX = -1;
-
         };
     }
     namespace math{
-        struct ARM: arm::Base{
+        struct CPU: cpu::Base{
             static constexpr Type TYPE = Type::math;
         };
     }
     namespace random{
-        struct ARM: arm::Base{
+        struct CPU: cpu::Base{
             static constexpr Type TYPE = Type::random;
         };
     }
     namespace logging{
-        struct ARM: arm::Base{
+        struct CPU: cpu::Base{
             static constexpr Type TYPE = Type::logging;
         };
     }
     template <typename T_SPEC>
-    struct ARM: Device<T_SPEC>, arm::Base{
-        template <typename OTHER_DEVICE>
-        static constexpr bool compatible = OTHER_DEVICE::DEVICE == DeviceId::ARM;
+    struct CPU: Device<T_SPEC>, cpu::Base{
         using SPEC = T_SPEC;
+        using EXECUTION_HINTS = typename SPEC::EXECUTION_HINTS;
         typename SPEC::LOGGING* logger = nullptr;
 #ifdef BACKPROP_TOOLS_DEBUG_CONTAINER_COUNT_MALLOC
         index_t malloc_counter = 0;
 #endif
     };
 
-    namespace arm{
-        template <typename T_SPEC>
-        struct Generic: ARM<T_SPEC>{};
-        template <typename T_SPEC>
-        struct DSP: ARM<T_SPEC>{};
-        template <typename T_SPEC>
-        struct OPT: ARM<T_SPEC>{};
-    }
-
-    using DefaultARMSpecification = arm::Specification<math::ARM, random::ARM, logging::ARM>;
-    using DefaultARM = arm::OPT<DefaultARMSpecification>;
+    using DefaultCPUSpecification = cpu::Specification<math::CPU, random::CPU, logging::CPU>;
+    using DefaultCPU = CPU<DefaultCPUSpecification>;
 }
 
 namespace backprop_tools{
-#ifdef BACKPROP_TOOLS_DEBUG_CONTAINER_COUNT_MALLOC
     template <typename DEV_SPEC, typename TI>
-    void count_malloc(devices::ARM<DEV_SPEC>& device, TI size){
+    void count_malloc(devices::CPU<DEV_SPEC>& device, TI size){
+#ifdef BACKPROP_TOOLS_DEBUG_CONTAINER_COUNT_MALLOC
         device.malloc_counter += size;
-    }
 #endif
+    }
 }
 
 #endif
---
+++ include/backprop_tools/devices/dummy.h
@@ -0,0 +1,7 @@
+#ifndef BACKPROP_TOOLS_DEVICES_DUMMY_H
+#define BACKPROP_TOOLS_DEVICES_DUMMY_H
+
+#include <backprop_tools/utils/generic/typing.h>
+#include "devices.h"
+namespace backprop_tools::devices{
+    namespace dum
