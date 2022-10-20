6eae02ad26257339c6e0331dde51856fc9183edf
LumoFace <lumoface@proton.me>
2022-10-20 15:21:43 +0000

    CSS refactoring

--- include/backprop_tools/nn/parameters/persist.h
+++ include/backprop_tools/nn/parameters/persist.h
@@ -6,4 +6,22 @@
 #include <highfive/H5Group.hpp>
 namespace backprop_tools{
     template<typename DEVICE, typename CONTAINER>
-    v
+    void save(DEVICE& device, nn::parameters::Plain::instance<CONTAINER>& parameter, HighFive::Group group) {
+        save(device, parameter.parameters, group, "parameters");
+    }
+    template<typename DEVICE, typename CONTAINER>
+    void save(DEVICE& device, nn::parameters::Gradient::instance<CONTAINER>& parameter, HighFive::Group group) {
+        save(device, (nn::parameters::Plain::instance<CONTAINER>&)parameter, group);
+        save(device, parameter.gradient, group, "gradient");
+    }
+    template<typename DEVICE, typename CONTAINER>
+    void load(DEVICE& device, nn::parameters::Plain::instance<CONTAINER>& parameter, HighFive::Group group) {
+        load(device, parameter.parameters, group, "parameters");
+    }
+    template<typename DEVICE, typename CONTAINER>
+    void load(DEVICE& device, nn::parameters::Gradient::instance<CONTAINER>& parameter, HighFive::Group group) {
+        load(device, (nn::parameters::Plain::instance<CONTAINER>&)parameter, group);
+        load(device, parameter.gradient, group, "gradient");
+    }
+}
+#endif
