fc4ff66f756ec2b458753f7067d7c6df611618b3
LumoFace <lumoface@proton.me>
2022-10-27 18:12:32 +0000

    Trying to fix travis build

--- include/backprop_tools/nn_models/mlp/operations_generic.h
+++ include/backprop_tools/nn_models/mlp/operations_generic.h
@@ -63,4 +63,39 @@ namespace backprop_tools {
                 INPUT_SPEC::ROWS == OUTPUT_SPEC::ROWS &&
                 OUTPUT_SPEC::COLS == MODEL_SPEC::OUTPUT_DIM &&
                 (!MODEL_SPEC::ENFORCE_FLOATING_POINT_TYPE || ( utils::typing::is_same_v<typename MODEL_SPEC::T, typename INPUT_SPEC::T> && utils::typing::is_same_v<typename INPUT_SPEC::T, typename OUTPUT_SPEC::T>));
-   
+    }
+    template<typename DEVICE, typename MODEL_SPEC, typename INPUT_SPEC, typename OUTPUT_SPEC, typename TEMP_SPEC>
+    void evaluate_memless(DEVICE& device, const nn_models::mlp::NeuralNetwork<MODEL_SPEC>& network, const Matrix<INPUT_SPEC>& input, Matrix<OUTPUT_SPEC>& output, Matrix<TEMP_SPEC>& layer_output_tick, Matrix<TEMP_SPEC>& layer_output_tock){
+        static_assert(nn_models::mlp::check_input_output<MODEL_SPEC, INPUT_SPEC, OUTPUT_SPEC>);
+        constexpr auto BATCH_SIZE = INPUT_SPEC::ROWS;
+        static_assert(TEMP_SPEC::ROWS >= BATCH_SIZE);
+        static_assert(TEMP_SPEC::COLS >= MODEL_SPEC::HIDDEN_DIM);
+        evaluate(device, network.input_layer, input, layer_output_tick);
+        for (typename DEVICE::index_t layer_i = 0; layer_i < MODEL_SPEC::NUM_HIDDEN_LAYERS; layer_i++){
+            if(layer_i % 2 == 0){
+                evaluate(device, network.hidden_layers[layer_i], layer_output_tick, layer_output_tock);
+            } else {
+                evaluate(device, network.hidden_layers[layer_i], layer_output_tock, layer_output_tick);
+            }
+        }
+        if constexpr(MODEL_SPEC::NUM_HIDDEN_LAYERS % 2 == 0){
+            evaluate(device, network.output_layer, layer_output_tick, output);
+        } else {
+            evaluate(device, network.output_layer, layer_output_tock, output);
+        }
+    }
+    template<typename DEVICE, typename MODEL_SPEC, typename INPUT_SPEC, typename OUTPUT_SPEC, typename BUFFER_MODEL_SPEC>
+    void evaluate(DEVICE& device, const nn_models::mlp::NeuralNetwork<MODEL_SPEC>& network, const Matrix<INPUT_SPEC>& input, Matrix<OUTPUT_SPEC>& output, nn_models::mlp::NeuralNetworkBuffers<BUFFER_MODEL_SPEC>& buffers){
+        static_assert(BUFFER_MODEL_SPEC::BATCH_SIZE == OUTPUT_SPEC::ROWS);
+        evaluate_memless(device, network, input, output, buffers.tick, buffers.tock);
+    }
+    template<typename DEVICE, typename MODEL_SPEC, typename INPUT_SPEC, typename OUTPUT_SPEC>
+    void evaluate(DEVICE& device, const nn_models::mlp::NeuralNetwork<MODEL_SPEC>& network, const Matrix<INPUT_SPEC>& input, Matrix<OUTPUT_SPEC>& output){
+        static_assert(nn_models::mlp::check_input_output<MODEL_SPEC, INPUT_SPEC, OUTPUT_SPEC>);
+        constexpr auto BATCH_SIZE = INPUT_SPEC::ROWS;
+        using T = typename MODEL_SPEC::T;
+        using TICK_TOCK_SPEC = matrix::Specification<T, typename DEVICE::index_t, BATCH_SIZE, MODEL_SPEC::HIDDEN_DIM>;
+#ifndef BACKPROP_TOOLS_DISABLE_DYNAMIC_MEMORY_ALLOCATIONS
+        MatrixDynamic<TICK_TOCK_SPEC> layer_output_tick;
+        MatrixDynamic<TICK_TOCK_SPEC> layer_output_tock;
+#else
