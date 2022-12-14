787c5dc6e0bd44cc99ad7a4711ef68f686d4d63e
LumoFace <lumoface@proton.me>
2022-12-14 16:01:19 +0000

    Command

--- include/backprop_tools/nn_models/mlp/operations_generic.h
+++ include/backprop_tools/nn_models/mlp/operations_generic.h
@@ -324,4 +324,10 @@ namespace backprop_tools {
     }
     template<typename TARGET_DEVICE, typename SOURCE_DEVICE,  typename TARGET_SPEC, typename SOURCE_SPEC>
     void copy(TARGET_DEVICE& target_device, SOURCE_DEVICE& source_device, nn_models::mlp::NeuralNetworkBuffersForwardBackward<TARGET_SPEC>& target, const nn_models::mlp::NeuralNetworkBuffersForwardBackward<SOURCE_SPEC>& source){
-        copy(target_device, source_device, (nn_models::mlp::NeuralNetworkBuffers<
+        copy(target_device, source_device, (nn_models::mlp::NeuralNetworkBuffers<TARGET_SPEC>&)target, (nn_models::mlp::NeuralNetworkBuffers<SOURCE_SPEC>&)source);
+        copy(target_device, source_device, target.d_input, source.d_input);
+        copy(target_device, source_device, target.d_output, source.d_output);
+    }
+}
+
+#endif
---
+++ include/backprop_tools/nn_models/mlp/persist_code.h
@@ -0,0 +1,9 @@
+
+#include "network.h"
+#include <backprop_tools/nn/optimizers/adam/persist_code.h>
+#include <backprop_tools/nn/layers/dense/persist_code.h>
+
+
+
+#include <string>
+#include <sst
