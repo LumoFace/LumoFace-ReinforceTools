e2f8a66ec4251155a5a14fefa39e7ab5037b8c37
LumoFace <lumoface@proton.me>
2023-03-02 15:02:09 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.

--- include/backprop_tools/rl/components/on_policy_runner/on_policy_runner.h
+++ include/backprop_tools/rl/components/on_policy_runner/on_policy_runner.h
@@ -31,4 +31,47 @@ namespace backprop_tools::rl::components{
             static constexpr TI STEPS_PER_ENV = T_SPEC::STEPS_PER_ENV;
             static constexpr TI STEPS_TOTAL = T_SPEC::STEPS_TOTAL;
             // structure: OBSERVATION - ACTION - ACTION_LOG_P - REWARD - TERMINATED - TRUNCATED - VALUE - ADVANTAGE - TARGEt_VALUE
-            static constexpr TI
+            static constexpr TI DATA_DIM = SPEC::ENVIRONMENT::OBSERVATION_DIM * 2 + SPEC::ENVIRONMENT::ACTION_DIM * 2 + 7;
+
+            // mem
+            // todo: evaluate transposing this / storing in column major order for better memory access in the single dimensional columns
+            typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<T, TI, STEPS_TOTAL + SPEC::N_ENVIRONMENTS, DATA_DIM>> data; // +1 * SPEC::N_ENVIRONMENTS for the final observation
+
+            // views
+            template<TI VIEW_DIM, bool ALL = false>
+            using DATA_VIEW = typename decltype(data)::template VIEW<STEPS_TOTAL + (ALL ? SPEC::N_ENVIRONMENTS : 0), VIEW_DIM>;
+
+            DATA_VIEW<SPEC::ENVIRONMENT::OBSERVATION_DIM, true> all_observations;
+            DATA_VIEW<SPEC::ENVIRONMENT::OBSERVATION_DIM> observations;
+            DATA_VIEW<SPEC::ENVIRONMENT::OBSERVATION_DIM, true> all_observations_normalized;
+            DATA_VIEW<SPEC::ENVIRONMENT::OBSERVATION_DIM> observations_normalized;
+            DATA_VIEW<SPEC::ENVIRONMENT::ACTION_DIM> actions_mean;
+            DATA_VIEW<SPEC::ENVIRONMENT::ACTION_DIM> actions;
+            DATA_VIEW<1> action_log_probs;
+            DATA_VIEW<1> rewards;
+            DATA_VIEW<1> terminated;
+            DATA_VIEW<1> truncated;
+            DATA_VIEW<1, true> all_values;
+            DATA_VIEW<1> values;
+            DATA_VIEW<1> advantages;
+            DATA_VIEW<1> target_values;
+        };
+        template <typename TI, TI T_NUM_THREADS>
+        struct ExecutionHints{
+            static constexpr TI NUM_THREADS = T_NUM_THREADS;
+        };
+    }
+
+    template <typename T_SPEC>
+    struct OnPolicyRunner{
+        using SPEC = T_SPEC;
+        using T = typename SPEC::T;
+        using TI = typename SPEC::TI;
+
+        TI step = 0;
+
+        typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<typename SPEC::ENVIRONMENT       , TI, 1, SPEC::N_ENVIRONMENTS, matrix::layouts::RowMajorAlignment<TI, 1>>> environments;
+        typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<typename SPEC::ENVIRONMENT::State, TI, 1, SPEC::N_ENVIRONMENTS, matrix::layouts::RowMajorAlignment<TI, 1>>> states;
+        typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<bool                             , TI, 1, SPEC::N_ENVIRONMENTS, matrix::layouts::RowMajorAlignment<TI, 1>>> truncated;
+        typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<TI                               , TI, 1, SPEC::N_ENVIRONMENTS, matrix::layouts::RowMajorAlignment<TI, 1>>> episode_step;
+        typename SPEC::CONTAINER_TYPE_TAG::template type<matrix::Specification<T                                , TI, 1, SPEC::N_ENVIRONMENTS, matr
