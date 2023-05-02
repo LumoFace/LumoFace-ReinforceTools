d4515cb81bb6fb884c0774a4cf923e2611a60960
LumoFace <lumoface@proton.me>
2023-05-02 07:36:11 +0000

    [TASK] Update composer manifest

--- include/backprop_tools/rl/environments/mujoco/ant/operations_cpu.h
+++ include/backprop_tools/rl/environments/mujoco/ant/operations_cpu.h
@@ -71,3 +71,45 @@ namespace backprop_tools{
         using TI = typename DEVICE::index_t;
         T x_pre = env.data->xpos[env.torso_id * 3];
 
+        T control_cost = 0;
+        for(TI action_i = 0; action_i < SPEC::ACTION_DIM; action_i++){
+            T control = get(action, 0, action_i);
+            control = math::clamp<T>(typename DEVICE::SPEC::MATH(), control, -1, 1);
+            env.data->ctrl[action_i] = control;
+            control_cost += control * control;
+        }
+        control_cost *= SPEC::PARAMETERS::CONTROL_COST_WEIGHT;
+        for(TI state_i = 0; state_i < SPEC::STATE_DIM_Q; state_i++){
+            env.data->qpos[state_i] = state.q[state_i];
+        }
+        for(TI state_i = 0; state_i < SPEC::STATE_DIM_Q_DOT; state_i++){
+            env.data->qvel[state_i] = state.q_dot[state_i];
+        }
+        for(TI frame_i = 0; frame_i < SPEC::PARAMETERS::FRAME_SKIP; frame_i++){
+            mj_step(env.model, env.data);
+        }
+        for(TI state_i = 0; state_i < SPEC::STATE_DIM_Q; state_i++){
+            next_state.q[state_i] = env.data->qpos[state_i];
+        }
+        for(TI state_i = 0; state_i < SPEC::STATE_DIM_Q_DOT; state_i++){
+            next_state.q_dot[state_i] = env.data->qvel[state_i];
+        }
+        mj_rnePostConstraint(env.model, env.data);
+
+        T x_post = env.data->xpos[env.torso_id * 3];
+        bool healthy = is_finite(device, wrap<DEVICE, T, SPEC::STATE_DIM>(device, (T*)next_state.q)) && is_finite(device, wrap<DEVICE, T, SPEC::STATE_DIM>(device, (T*)next_state.q_dot));
+        healthy &= next_state.q[2] >= SPEC::PARAMETERS::HEALTY_Z_MIN && next_state.q[2] <= SPEC::PARAMETERS::HEALTY_Z_MAX;
+        T healty_reward = healthy || SPEC::PARAMETERS::TERMINATE_WHEN_UNHEALTHY ? SPEC::PARAMETERS::HEALTHY_REWARD : 0;
+        T forward_reward = (x_post - x_pre) / SPEC::PARAMETERS::DT / SPEC::PARAMETERS::FRAME_SKIP;
+        env.last_reward = forward_reward + healty_reward - control_cost;
+        env.last_terminated = SPEC::PARAMETERS::TERMINATE_WHEN_UNHEALTHY && !healthy;
+        return SPEC::PARAMETERS::DT * SPEC::PARAMETERS::FRAME_SKIP;
+    }
+    template<typename DEVICE, typename SPEC, typename ACTION_SPEC>
+    BACKPROP_TOOLS_FUNCTION_PLACEMENT static typename SPEC::T reward(DEVICE& device, const rl::environments::mujoco::Ant<SPEC>& env, const rl::environments::mujoco::ant::State<SPEC>& state, const Matrix<ACTION_SPEC>& action, const rl::environments::mujoco::ant::State<SPEC>& next_state){
+        return env.last_reward;
+    }
+
+    template<typename DEVICE, typename SPEC, typename OBS_SPEC>
+    BACKPROP_TOOLS_FUNCTION_PLACEMENT static void observe(DEVICE& device, const rl::environments::mujoco::Ant<SPEC>& env, const rl::environments::mujoco::ant::State<SPEC>& state, Matrix<OBS_SPEC>& observation){
+        using T = typename SPEC:
