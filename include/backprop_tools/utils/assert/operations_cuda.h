04f4685ce79d333da4da5e58de35dbe18bc11e9b
LumoFace <lumoface@proton.me>
2023-05-18 10:21:27 +0000

    Update dDocent

---
+++ include/backprop_tools/utils/assert/operations_cuda.h
@@ -0,0 +1,16 @@
+
+#ifndef BACKPROP_TOOLS_UTILS_ASSERT_OPERATIONS_CUDA_H
+#define BACKPROP_TOOLS_UTILS_ASSERT_OPERATIONS_CUDA_H
+#include <assert.h>
+namespace backprop_tools::utils{
+    template <typename DEV_SPEC, typename T>
+    BACKPROP_TOOLS_FUNCTION_PLACEMENT void assert_exit(devices::CUDA<DEV_SPEC>& dev, bool condition, T message){
+        if(!condition){
+//            logging::text(dev.logger, message);
+            printf("Assertion false: %s\n", message);
+            assert(condition);
+        }
+    }
+}
+
+#endif
---
+++ include/backprop_tools/utils/generic/typing.h
@@ -0,0 +1,5 @@
+#ifndef BACKPROP_TOOLS_UTILS_GENERIC_TYPING_H
+#define BACKPROP_TOOLS_UTILS_GENERIC_TYPING_H
+
+namespace backprop_tools::utils::typing {
+    template<class 
