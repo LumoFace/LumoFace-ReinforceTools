3b58293ff902f2bc157bcdb99e715b4b8a4bd465
LumoFace <lumoface@proton.me>
2023-08-15 09:46:01 +0000

    updating image

--- tests/src/container/persist_code_store.cpp
+++ tests/src/container/persist_code_store.cpp
@@ -28,4 +28,57 @@ TEST(BACKPROP_TOOLS_CONTAINER_PERSIST_CODE_STORE, TEST){
     bpt::malloc(device, m);
     bpt::randn(device, m, rng);
     bpt::print(device, m);
-    auto output 
+    auto output = bpt::save(device, m, "matrix_1", const_declaration);
+    std::cout << "output: " << output << std::endl;
+    std::filesystem::create_directories("data");
+    std::ofstream file;
+    file.open ("data/test_backprop_tools_container_persist_matrix.h");
+    file << output;
+    file.close();
+
+    ASSERT_TRUE(true);
+}
+
+TEST(BACKPROP_TOOLS_CONTAINER_PERSIST_CODE_STORE, TEST_DENSE_LAYER){
+    using DEVICE = bpt::devices::DefaultCPU;
+    using DTYPE = float;
+    using OPTIMIZER_PARAMETERS = bpt::nn::optimizers::adam::DefaultParametersTorch<DTYPE>;
+    using OPTIMIZER = bpt::nn::optimizers::Adam<OPTIMIZER_PARAMETERS>;
+    OPTIMIZER optimizer;
+    DEVICE device;
+    auto rng = bpt::random::default_engine(DEVICE::SPEC::RANDOM());
+    bpt::nn::layers::dense::LayerBackwardGradient<bpt::nn::layers::dense::Specification<DTYPE, typename DEVICE::index_t, 3, 3, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::parameters::Adam>> layer;
+    bpt::malloc(device, layer);
+    bpt::init_kaiming(device, layer, rng);
+    bpt::zero_gradient(device, layer);
+    bpt::reset_forward_state(device, layer);
+    bpt::reset_optimizer_state(device, layer, optimizer);
+    bpt::randn(device, layer.weights.gradient, rng);
+    bpt::randn(device, layer.weights.gradient_first_order_moment, rng);
+    bpt::randn(device, layer.weights.gradient_second_order_moment, rng);
+    bpt::randn(device, layer.biases.gradient, rng);
+    bpt::randn(device, layer.biases.gradient_first_order_moment, rng);
+    bpt::randn(device, layer.biases.gradient_second_order_moment, rng);
+    auto output = bpt::save(device, layer, "layer_1", const_declaration);
+    std::cout << "output: " << output << std::endl;
+    std::filesystem::create_directories("data");
+    std::ofstream file;
+    file.open("data/test_backprop_tools_nn_layers_dense_persist_code.h");
+    file << output;
+    file.close();
+
+    ASSERT_TRUE(true);
+}
+
+TEST(BACKPROP_TOOLS_CONTAINER_PERSIST_CODE_STORE, TEST_MLP){
+    using DEVICE = bpt::devices::DefaultCPU;
+    using DTYPE = float;
+    DEVICE device;
+    auto rng = bpt::random::default_engine(DEVICE::SPEC::RANDOM());
+    using SPEC = bpt::nn_models::mlp::InferenceSpecification<bpt::nn_models::mlp::StructureSpecification<DTYPE, typename DEVICE::index_t, 13, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1, bpt::MatrixDynamicTag, true, bpt::matrix::layouts::RowMajorAlignment<typename DEVICE::index_t, 1>>>;
+    bpt::nn_models::mlp::NeuralNetwork<SPEC> mlp;
+    bpt::malloc(device, mlp);
+    bpt::init_weights(device, mlp, rng);
+    auto output = bpt::save(device, mlp, "mlp_1", const_declaration);
+    std::cout << "output: " << output << std::endl;
+  
