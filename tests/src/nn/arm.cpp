d84ce2cead4dd3d02377d7d66a44073528ea302c
LumoFace <lumoface@proton.me>
2023-08-18 10:06:11 +0000

    ip_tunnel: fix ip_tunnel_lookup

--- tests/src/nn/arm.cpp
+++ tests/src/nn/arm.cpp
@@ -70,4 +70,39 @@ void test_mlp_forward() {
     auto rng = bpt::random::default_engine(DEVICE::SPEC::RANDOM());
     using STRUCTURE_SPEC = bpt::nn_models::mlp::StructureSpecification<DTYPE, typename DEVICE::index_t, INPUT_DIM, OUTPUT_DIM, N_HIDDEN_LAYERS, HIDDEN_DIM, HIDDEN_ACTIVATION_FUNCTION, ACTIVATION_FUNCTION, 1,  bpt::MatrixDynamicTag, true, bpt::matrix::layouts::RowMajorAlignment<typename DEVICE::index_t, 1>>;
     using SPEC = bpt::nn_models::mlp::BackwardGradientSpecification<STRUCTURE_SPEC>;
-    using TYPE = bpt::nn_models::mlp::NeuralNetworkBackwa
+    using TYPE = bpt::nn_models::mlp::NeuralNetworkBackwardGradient<SPEC>;
+    using FORWARD_BACKWARD_BUFFERS = typename TYPE::template BuffersForwardBackward<BATCH_SIZE>;
+    TYPE mlp_cpu, mlp_arm;
+    FORWARD_BACKWARD_BUFFERS buffers;
+    bpt::malloc(device, mlp_cpu);
+    bpt::malloc(device, mlp_arm);
+    bpt::malloc(device, buffers);
+    bpt::init_weights(device, mlp_cpu, rng);
+    bpt::zero_gradient(device, mlp_cpu);
+    bpt::copy(device, device, mlp_arm, mlp_cpu);
+
+    bpt::MatrixDynamic<bpt::matrix::Specification<DTYPE, typename DEVICE::index_t, BATCH_SIZE, SPEC::STRUCTURE_SPEC::INPUT_DIM>> input;
+    bpt::malloc(device, input);
+    bpt::randn(device, input, rng);
+    bpt::forward(device, mlp_cpu, input);
+    bpt::forward(device_arm, mlp_arm, input);
+    bpt::print(device, mlp_arm.output_layer.output);
+
+    auto abs_diff_output = bpt::abs_diff(device, mlp_arm.output_layer.output, mlp_arm.output_layer.output);
+    auto abs_diff_network = bpt::abs_diff(device, mlp_arm, mlp_cpu);
+
+    ASSERT_LT(abs_diff_output, 1e-5);
+    ASSERT_LT(abs_diff_network, 1e-5);
+}
+
+TEST(BACKPROP_TOOLS_NN_ARM, TEST_MLP_FORWARD){
+    test_mlp_forward<double, 13, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 1, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 1, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 1, 1, 2, 1, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 4, 2, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 4, 3, 1, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 4, 30, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::nn::activation_functions::ActivationFunction::IDENTITY, 1>();
+    test_mlp_forward<double, 13, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::IDENTITY, bpt::nn::activation_functions::ActivationFunction::RELU, 1>();
+    test_mlp_forward<double, 13, 4, 3, 64, bpt::nn::activation_functions::ActivationFunction::RELU, bpt::n
