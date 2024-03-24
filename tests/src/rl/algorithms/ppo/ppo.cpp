81095b43f78c9be9169fb054a18d6fce7da6f46d
LumoFace <lumoface@proton.me>
2024-03-24 12:10:36 +0000

    add npmignore

--- tests/src/rl/algorithms/ppo/ppo.cpp
+++ tests/src/rl/algorithms/ppo/ppo.cpp
@@ -48,4 +48,47 @@ TEST(BACKPROP_TOOLS_RL_ALGORITHMS_PPO, TEST){
 
     penv::ENVIRONMENT envs[prl::N_ENVIRONMENTS];
     bpt::init(device, on_policy_runner, envs, rng);
-    bpt::init(device, ppo, act
+    bpt::init(device, ppo, actor_optimizer, critic_optimizer, rng);
+    device.logger = &logger;
+    bpt::construct(device, device.logger);
+    auto training_start = std::chrono::high_resolution_clock::now();
+    for(TI ppo_step_i = 0; ppo_step_i < 1000; ppo_step_i++) {
+        device.logger->step = on_policy_runner.step;
+
+        if(ppo_step_i % 100 == 0){
+            std::chrono::duration<T> training_elapsed = std::chrono::high_resolution_clock::now() - training_start;
+            std::cout << "PPO step: " << ppo_step_i << " elapsed: " << training_elapsed.count() << "s" << std::endl;
+            bpt::add_scalar(device, device.logger, "ppo/step", ppo_step_i);
+        }
+        for (TI action_i = 0; action_i < penv::ENVIRONMENT::ACTION_DIM; action_i++) {
+            T action_log_std = bpt::get(ppo.actor.log_std.parameters, 0, action_i);
+            std::stringstream topic;
+            topic << "actor/action_std/" << action_i;
+            bpt::add_scalar(device, device.logger, topic.str(), bpt::math::exp(DEVICE::SPEC::MATH(), action_log_std));
+        }
+        auto start = std::chrono::high_resolution_clock::now();
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            bpt::collect(device, on_policy_runner_dataset, on_policy_runner, ppo.actor, actor_eval_buffers, rng);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<T> elapsed = end - start;
+//            std::cout << "Rollout: " << elapsed.count() << " s" << std::endl;
+        }
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            evaluate(device, ppo.critic, on_policy_runner_dataset.all_observations, on_policy_runner_dataset.all_values, critic_buffers_all);
+            bpt::estimate_generalized_advantages(device, on_policy_runner_dataset, prl::PPO_SPEC::PARAMETERS{});
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<T> elapsed = end - start;
+//            std::cout << "GAE: " << elapsed.count() << " s" << std::endl;
+        }
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            bpt::train(device, ppo, on_policy_runner_dataset, actor_optimizer, critic_optimizer, ppo_buffers, actor_buffers, critic_buffers, rng);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<T> elapsed = end - start;
+//            std::cout << "Train: " << elapsed.count() << " s" << std::endl;
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<T> elapsed = end - start;
+//        std::cout << "Total: " << elapsed.coun
