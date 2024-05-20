09666ddb4898ceb80877723e08bafcdcb29d43b2
LumoFace <lumoface@proton.me>
2024-05-20 05:49:41 +0000

    And another one

--- tests/src/utils/nn_comparison_mlp.h
+++ tests/src/utils/nn_comparison_mlp.h
@@ -18,4 +18,39 @@ typename SPEC::T abs_diff(DEVICE& device, const backprop_tools::nn_models::mlp::
 template <typename DEVICE, typename SPEC>
 typename SPEC::T abs_diff_grad(DEVICE& device, const backprop_tools::nn_models::mlp::NeuralNetworkBackwardGradient<SPEC>& n1, const backprop_tools::nn_models::mlp::NeuralNetworkBackwardGradient<SPEC>& n2) {
     using NetworkType = typename std::remove_reference<decltype(n1)>::type;
-    typedef
+    typedef typename SPEC::T T;
+    using GradNetworkSpec = backprop_tools::nn_models::mlp::BackwardGradientSpecification<typename SPEC::STRUCTURE_SPEC>;
+    using GradNetworkType = backprop_tools::nn_models::mlp::NeuralNetworkBackwardGradient<GradNetworkSpec>;
+    GradNetworkType n1g;
+    bpt::malloc(device, n1g);
+    bpt::copy(device, device, n1g, n1);
+    bpt::reset_forward_state(device, n1g);
+    GradNetworkType n2g;
+    bpt::malloc(device, n2g);
+    bpt::copy(device, device, n2g, n2);
+    bpt::reset_forward_state(device, n2g);
+    T acc = 0;
+    acc += bpt::abs_diff(device, n1g.input_layer, n2g.input_layer);
+    for(typename DEVICE::index_t layer_i = 0; layer_i < NetworkType::NUM_HIDDEN_LAYERS; layer_i++) {
+        acc += bpt::abs_diff(device, n1g.hidden_layers[layer_i], n2g.hidden_layers[layer_i]);
+    }
+    acc += bpt::abs_diff(device, n1g.output_layer, n2g.output_layer);
+    bpt::free(device, n1g);
+    bpt::free(device, n2g);
+    return acc;
+}
+
+template <typename DEVICE, typename SPEC>
+typename SPEC::T abs_diff_adam(DEVICE& device, const backprop_tools::nn_models::mlp::NeuralNetworkAdam<SPEC>& n1, const backprop_tools::nn_models::mlp::NeuralNetworkAdam<SPEC>& n2) {
+    using NetworkType = typename std::remove_reference<decltype(n1)>::type;
+    typedef typename SPEC::T T;
+    T acc = 0;
+    acc += bpt::abs_diff(device, n1.input_layer, n2.input_layer);
+    for(typename DEVICE::index_t layer_i = 0; layer_i < NetworkType::NUM_HIDDEN_LAYERS; layer_i++) {
+        acc += bpt::abs_diff(device, n1.hidden_layers[layer_i], n2.hidden_layers[layer_i]);
+    }
+    acc += bpt::abs_diff(device, n1.output_layer, n2.output_layer);
+    return acc;
+}
+
+#endif
---
+++ tests/src/utils/utils.h
@@ -0,0 +1,37 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+#include <vector>
+
+#define BACKPROP_TOOLS_STRINGIZE(x) #x
+#define BACKPROP_TOOLS_MACRO_TO_STR(macro) BACKPROP_TOOLS_STRINGIZE(macro)
+
+template <typename T, int DIM>
+void standardise(const T input[DIM], const T mean[DIM], const T std[DIM], T output[DIM]){
+    for (int i = 0; i < DIM; i++){
+        output[i] = (input[i] - mean[i]) / std[i];
+    }
+}
+template <typename T, typename SPEC>
+T abs_diff_matrix(const backprop_tools::Matrix<SPEC> A, const std::vector<std::vector<T>>& B) {
+    T acc = 0;
+    for (int i = 0; i < SPEC::ROWS; i++){
+        for (int j = 0; j < SPEC::COLS; j++){
+            acc += std::abs(get(A, i, j) - B[i][j]);
+        }
+    }
+    return acc;
+}
+
+template <typename T, typename SPEC>
+T abs_diff_matrix(backprop_tools::Matrix<SPEC> A, const T* B) {
+    T acc = 0;
+    for (int i = 0; i < SPEC::ROWS; i++){
+        for (int j = 0; j < SPEC::COLS; j++){
+            acc += std::abs(get(A, i, j) - B[i * SPEC::COLS + j]);
+        }
+    }
+    return acc;
+}
+
+template <typename T, int N_ROWS>
+T abs_diff_vector(const T A[N_ROWS], const T B[
